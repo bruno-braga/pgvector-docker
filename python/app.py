4d394e1fa8d216e09129f4caf23afb19675dbb48
Bruno <brunobraga@protonmail.com>
2024-11-11 22:39:21 -0300

    Add connection to database

--- python/app.py
+++ python/app.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+load_dotenv()
+
+
+conn = psycopg2.connect(
+    database=os.getenv('DB_DATABASE'),
+    user=os.getenv('DB_USERNAME'),
+    host=os.getenv('DB_HOST'),
+    password=os.getenv('DB_PASSWORD'),
+    port=os.getenv('DB_PORT')
+)
+
+cur = conn.cursor()
+cur.execute("""SELECT * FROM items;""")
+
+print(cur.fetchall())
+
+conn.close()
