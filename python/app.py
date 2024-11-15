4b57fbf092cfe83a9039c635d18acdedc219a43b
Bruno <brunobraga@protonmail.com>
2024-11-14 22:38:52 -0300

    Adapt script to save embedding using pgvector

--- python/app.py
+++ python/app.py
@@ -1,21 +1,64 @@
-from dotenv import load_dotenv
-import os
-import psycopg2
+import tqdm
+import numpy as np
+from PyPDF2 import PdfReader
+from psycopg2.extras import Json
+from pgvector.psycopg2 import register_vector
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
 
-load_dotenv()
+import db
 
 
-conn = psycopg2.connect(
-    database=os.getenv('DB_DATABASE'),
-    user=os.getenv('DB_USERNAME'),
-    host=os.getenv('DB_HOST'),
-    password=os.getenv('DB_PASSWORD'),
-    port=os.getenv('DB_PORT')
-)
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-cur = conn.cursor()
-cur.execute("""SELECT * FROM items;""")
+cur = db.conn.cursor()
+register_vector(db.conn)
 
-print(cur.fetchall())
+reader = PdfReader("./files/artigo_exemplo.pdf")
+    
+text = ""
 
-conn.close()
+for page in reader.pages:
+    text += page.extract_text() + "\n"
+        
+text.split("\n")[100:105]
+
+
+def merge_lines(text):
+    merged = []
+    current_sentence = ""
+    
+    for line in text:
+        line = line.strip()
+        if not line:
+            continue
+            
+        if current_sentence:
+            line = ' ' + line
+            
+        current_sentence += line
+        
+        if ". " in current_sentence:
+            parts = current_sentence.split(". ")
+            for part in parts[:-1]:
+                merged.append(part + ".")
+            current_sentence = parts[-1]
+    
+    if current_sentence:
+        merged.append(current_sentence)
+        
+    return merged
+
+sentences = merge_lines(text.split("\n"))
+# sentences[0:5]
+
+embeddings = model.encode(sentences)
+
+insert = "INSERT INTO embeddings (value) VALUES (%s::vector)"
+
+for idx, embedding in tqdm(enumerate(embeddings), total=len(embeddings)):
+    embedding_list = embedding.tolist()
+    cur.execute(insert, (embedding_list,))
+
+db.conn.commit()
+db.conn.close()
