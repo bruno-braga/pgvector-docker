e8158fb86114a94e9b15a389a49361fd0d6ec304
Bruno <brunobraga@protonmail.com>
2024-11-24 20:32:34 -0300

    Paarse html instead of pdf

--- python/populate_database.py
+++ python/populate_database.py
@@ -3,43 +3,13 @@ from sentence_transformers import SentenceTransformer
 from pgvector.psycopg2 import register_vector
 
 import db
-
-def merge_lines(text):
-    merged = []
-    current_sentence = ""
-    
-    for line in text:
-        line = line.strip()
-        if not line:
-            continue
-            
-        if current_sentence:
-            line = ' ' + line
-            
-        current_sentence += line
-        
-        if ". " in current_sentence:
-            parts = current_sentence.split(". ")
-            for part in parts[:-1]:
-                merged.append(part + ".")
-            current_sentence = parts[-1]
-    
-    if current_sentence:
-        merged.append(current_sentence)
-        
-    return merged
+import parser
 
 register_vector(db.conn)
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-reader = PdfReader("./files/artigo_exemplo.pdf")
-text = ""
-
-for page in reader.pages:
-    text += page.extract_text() + "\n"
-        
-sentences = merge_lines(text.split("\n"))
+title, sentences = parser.extract_arxiv_content("./tosrag.html")
 
 embeddings = model.encode(sentences)
 
