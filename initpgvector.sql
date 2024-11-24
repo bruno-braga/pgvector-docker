7e7f9950d912b9264d9c343be98f0d0e4d1fae94
Bruno <brunobraga@protonmail.com>
2024-11-24 20:32:06 -0300

    Add article title

--- initpgvector.sql
+++ initpgvector.sql
@@ -3,10 +3,11 @@ CREATE EXTENSION vector;
 CREATE TABLE items (
     id bigserial PRIMARY KEY,
     text text,
+    article_title text,
     embedding vector
 );
 
-INSERT INTO items (text, embedding) VALUES ('Hello', '[1,2,3]'), ('World', '[4,5,6]');
+INSERT INTO items (text, page_number, embedding) VALUES ('Hello', 1, '[1,2,3]'), ('World', 2, '[4,5,6]');
 
 SELECT * FROM items ORDER BY embedding <-> '[3,1,2]' LIMIT 5;
 
