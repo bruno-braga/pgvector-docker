84981701c61610eb88daf95d9cbfa25cf96e6e24
Bruno <brunobraga@protonmail.com>
2024-11-25 21:05:51 -0300

    Add strategy to parse files

--- python/populate_database.py
+++ python/populate_database.py
@@ -2,21 +2,27 @@ from PyPDF2 import PdfReader
 from sentence_transformers import SentenceTransformer
 from pgvector.psycopg2 import register_vector
 
+import os
 import db
-import parser
+
+from parser import Context
 
 register_vector(db.conn)
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-title, sentences = parser.extract_arxiv_content("./tosrag.html")
+for folder in os.listdir('./articles'):
+    for file in os.listdir(os.path.join('./articles', folder)):
+        if file.endswith(".html"):
+            context = Context(folder)
+            title, sentences = context.extract(os.path.join('./articles', folder, file))
 
-embeddings = model.encode(sentences)
+            embeddings = model.encode(sentences)
 
-data = []
-for embedding, sentence in zip(embeddings, sentences):
-    embedding_list = embedding.tolist()
-    data_tuple = (sentence, embedding_list)
-    data.append(data_tuple)
+            data = []
+            for embedding, sentence in zip(embeddings, sentences):
+                embedding_list = embedding.tolist()
+                data_tuple = (sentence, embedding_list, title)
+                data.append(data_tuple)
 
-db.insert_items(data)
+            db.insert_items(data)
