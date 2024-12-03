35d75f26f2175db32c44dd2051fbff5f0fc6f95f
Bruno <brunobraga@protonmail.com>
2024-12-03 20:24:51 -0300

    Removing files that are now in the rag folder

--- app/api/runner.py
+++ app/api/runner.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from api import init_app
 
 app = Flask(__name__)
--- app/text.py
+++
@@ -1,4 +0,0 @@
-from TTS.api import TTS
-
-tts = TTS(model_name="tts_models/en/vctk/vits")
-tts.tts_to_file(text="Hello! This is an example.", file_path="output.wav")
