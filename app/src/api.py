21e7111c84657f03d8f70dfbafdef8697b15e4ec
Bruno <brunobraga@protonmail.com>
2024-12-03 22:16:17 -0300

    Rename controller to blueprints to maitain flask terminology

--- app/src/api.py
+++ app/src/api.py
@@ -1,6 +1,6 @@
 from flask_inertia import Inertia
 
-from Http.Controller.HomeController import home_bp
+from Http.Blueprints.Home import home_bp
 
 def init_app(app):
     app.register_blueprint(home_bp)
