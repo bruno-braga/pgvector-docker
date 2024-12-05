17028cb496e98eddd50be4b4a6e4463a953cb4fb
Bruno <brunobraga@protonmail.com>
2024-12-04 22:13:08 -0300

    Add and configure SQLAlchemy

--- app/src/api.py
+++ app/src/api.py
@@ -1,11 +1,18 @@
 from flask_inertia import Inertia
+from flask_sqlalchemy import SQLAlchemy
+
+from database.db_singleton import db
 
 from Http.Blueprints.Home import home_bp
+from Http.Blueprints.Rag import rag_bp
 
 def init_app(app):
     app.register_blueprint(home_bp)
+    app.register_blueprint(rag_bp, url_prefix='/rag')
 
     inertia = Inertia()
     inertia.init_app(app)
+
+    db.init_app(app)
 
     return app
--- app/src/runner.py
+++ app/src/runner.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from api import init_app
 
-app = Flask(__name__, template_folder='static/')
+import os
 
-app = init_app(app)
+app = Flask(__name__, template_folder='static/')
 
 app.config['SECRET_KEY'] = "secret!"
 app.config['INERTIA_TEMPLATE'] = "base.html"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app = init_app(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
